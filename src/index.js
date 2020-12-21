c5253228ac02f57866fcf8a2e80ac5d9151e4453
Elias Moura <d.eliasmoura2@gmail.com>
2020-12-21 15:22:51 -0300

    modifiying the index, a link to github was added

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*
+  // @ts-check
+*/
+// to check data types
+
+/**
+ * @file index.js the main file
+ * @author Elias Moura
+ * @see <a href="https://github.com/EliasMoura2"> Github Elias Moura</a>
+ */
+
+/**
+ * 
+ * This is the fullname of the user
+ * @type {string}
+ * 
+ */
+const fullName = 'Elias Moura'
+// si ponemos un valor numerico => type 'number' is not assignable to type 'string'
+
