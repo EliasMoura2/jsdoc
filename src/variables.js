5cb807e8d5ffa3e2c5ee9bf2480b62f9ce3fc885
Elias Moura <d.eliasmoura2@gmail.com>
2020-12-21 10:50:08 -0300

    documentation for variables

---
+++ src/variables.js
@@ -0,0 +1,14 @@
+/*
+  // @ts-check
+*/
+// para verificar los tipos de datos
+
+/**
+ * 
+ * This is the fullname of the user
+ * @type {string}
+ * 
+ */
+const fullName = 'Elias Moura'
+// si ponemos un valor numerico => type 'number' is not assignable to type 'string'
+
