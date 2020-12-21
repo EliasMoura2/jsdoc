1fd32a7e40d0f0dd3eed6a61e8961b7b6f28a4a0
Elias Moura <d.eliasmoura2@gmail.com>
2020-12-21 10:50:24 -0300

    documentation for arrays

---
+++ src/arrays.js
@@ -0,0 +1,26 @@
+//@ts-check
+/**
+ * array of users ages
+ * @type {Array} 
+ * */
+const age = [20,45,30,65]
+
+/**
+ * array of users scores
+ * @type {Array<Number>} 
+ */
+const scores = [10, 45.50, 190.99, 65]
+// const scores = [10, 45.50, 190.99, 65, true] 
+// type boolean is not assignable to type number
+
+/** 
+ * Array of Numbers and strings
+ * @type {Array<Number | string>}
+*/
+const scoresAndStrings = [10, "20", 35.50, "45"]
+
+/**
+ * Array of names
+ * @type {Array<string>}
+ */
+const names = ['Elias', 'Milagros', 'Alejandra', 'Damian']
