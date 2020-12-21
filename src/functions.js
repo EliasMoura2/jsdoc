0f68262d348b687a804a6cd734615f8d5d99a3de
Elias Moura <d.eliasmoura2@gmail.com>
2020-12-21 11:04:59 -0300

    documentation for functions

---
+++ src/functions.js
@@ -0,0 +1,25 @@
+//@ts-check
+
+// normal funciton
+
+/**
+ * Add numbers two numbers and return a Number
+ * @param {Number} n1 first number
+ * @param {Number} n2 second number
+ * @return {Number} total result
+ */
+function add(n1, n2){
+  return n1 + n2
+}
+// @return {void}
+// Type number is not assignable to type void
+
+// arrow function
+/**
+ * substract two numbers and return a string
+ * @param {Number} n1 first number
+ * @param {Number} n2 second number
+ * @returns {string}
+ */
+const substract = (n1, n2) => `The result is: ${n1 - n2}`
+
